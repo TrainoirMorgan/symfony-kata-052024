f4728670b9b060c6a1cac7786c8761ed99222db1
Morgan Trainoir <morgantrainoir.pro@gmail.com>
2024-05-17 13:40:03 +0000

    configure fields admin book

--- src/Controller/Admin/BookCrudController.php
+++ src/Controller/Admin/BookCrudController.php
@@ -3,10 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Book;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class BookCrudController extends AbstractCrudController
 {
@@ -15,14 +17,16 @@ class BookCrudController extends AbstractCrudController
         return Book::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield AssociationField::new('addedBy');
+        yield TextField::new('title');
+        yield TextField::new('author');
+        yield DateTimeField::new('publishedDate');
+        yield TextField::new('isbn');
+        yield DateTimeField::new('createdAt');
+        yield DateTimeField::new('updatedAt');
     }
-    */
+    
 }
